afeeb6fce7b1c33edfec4d2b679015c92b3ede59
Vladimir Brik <vbrik@icecube.wisc.edu>
2023-07-27 12:50:56 -0500

    add mailman-to-google-group-members-import.py

--- mailman-to-google-group-settings-import.py
+++ mailman-to-google-group-settings-import.py
@@ -9,6 +9,8 @@ from google.oauth2 import service_account
 from googleapiclient import discovery
 from googleapiclient.errors import HttpError
 
+from utils import get_google_group_config_from_mailman_config
+
 
 handler = colorlog.StreamHandler()
 handler.setFormatter(colorlog.ColoredFormatter("%(log_color)s%(levelname)s:%(message)s"))
@@ -17,89 +19,13 @@ logger.propagate = False
 logger.addHandler(handler)
 
 
-def get_google_group_config_from_mailman_config(mmcfg):
-    # https://developers.google.com/admin-sdk/groups-settings/v1/reference/groups#json
-    if mmcfg["advertised"] and mmcfg["archive"]:
-        if mmcfg["archive_private"]:
-            who_can_view_group = "ALL_MEMBERS_CAN_VIEW"
-        else:
-            who_can_view_group = "ALL_IN_DOMAIN_CAN_VIEW"
-    else:  # not advertised or not archived
-        who_can_view_group = "ALL_MANAGERS_CAN_VIEW"
-
-    if mmcfg["generic_nonmember_action"] in (0, 1):  # accept, hold
-        who_can_post_message = "ANYONE_CAN_POST"
-    else:  # reject, discard
-        who_can_post_message = "ALL_MEMBERS_CAN_POST"
-    if mmcfg["default_member_moderation"] and mmcfg["member_moderation_action"] in (
-        1,
-        2,
-    ):  # reject or discard
-        who_can_post_message = "NONE_CAN_POST"
-
-    if mmcfg["generic_nonmember_action"] == 0:  # accept
-        message_moderation_level = "MODERATE_NONE"
-    else:  # hold, reject, discard
-        message_moderation_level = "MODERATE_NON_MEMBERS"
-    if mmcfg["default_member_moderation"]:
-        message_moderation_level = "MODERATE_ALL_MESSAGES"
-
-    if mmcfg["private_roster"] == 0:
-        who_can_view_membership = "ALL_IN_DOMAIN_CAN_VIEW"
-    elif mmcfg["private_roster"] == 1:
-        who_can_view_membership = "ALL_MEMBERS_CAN_VIEW"
-    else:
-        who_can_view_membership = "ALL_MANAGERS_CAN_VIEW"
-
-    ggcfg = {
-        "email": mmcfg["email"],
-        "name": mmcfg["real_name"],
-        "description": (
-            mmcfg["description"] + "\n" + mmcfg["info"] if mmcfg["info"] else mmcfg["description"]
-        ),
-        "whoCanJoin": "CAN_REQUEST_TO_JOIN",
-        "whoCanViewMembership": who_can_view_membership,
-        "whoCanViewGroup": who_can_view_group,
-        "allowExternalMembers": "true",  # can't be tighter until we start forcing people to use @iwe addresses
-        "whoCanPostMessage": who_can_post_message,
-        "allowWebPosting": "true",
-        "primaryLanguage": "en",
-        "isArchived": ("true" if mmcfg["archive"] else "false"),
-        "archiveOnly": "false",
-        "messageModerationLevel": message_moderation_level,
-        "spamModerationLevel": "MODERATE",  # this is the default
-        "replyTo": "REPLY_TO_IGNORE",  # users individually decide where the message reply is sent
-        # "customReplyTo": "",  # only if replyTo is REPLY_TO_CUSTOM
-        "includeCustomFooter": "false",
-        # "customFooterText": ""  # only if includeCustomFooter,
-        "sendMessageDenyNotification": "false",
-        # "defaultMessageDenyNotificationText": "",  # only matters if sendMessageDenyNotification is true
-        "membersCanPostAsTheGroup": "false",
-        "includeInGlobalAddressList": "false",  # has to do with Outlook integration
-        "whoCanLeaveGroup": ("ALL_MEMBERS_CAN_LEAVE" if mmcfg["unsubscribe_policy"] else "NONE_CAN_LEAVE"),
-        "whoCanContactOwner": "ALL_IN_DOMAIN_CAN_CONTACT",
-        "favoriteRepliesOnTop": "false",
-        "whoCanApproveMembers": "ALL_MANAGERS_CAN_APPROVE",
-        "whoCanBanUsers": "OWNERS_AND_MANAGERS",
-        "whoCanModerateMembers": "OWNERS_AND_MANAGERS",
-        "whoCanModerateContent": "OWNERS_AND_MANAGERS",
-        "whoCanAssistContent": "NONE",  # has something to do with collaborative inbox
-        "enableCollaborativeInbox": "false",
-        "whoCanDiscoverGroup": (
-            "ALL_IN_DOMAIN_CAN_DISCOVER" if mmcfg["advertised"] else "ALL_MEMBERS_CAN_DISCOVER"
-        ),
-        "defaultSender": "DEFAULT_SELF",
-    }
-    return ggcfg
-
-
 def set_controlled_mailing_list_setting(ggcfg):
-    def _override(ggcfg, key, value):
-        if key not in ggcfg:
+    def _override(cfg, key, value):
+        if key not in cfg:
             logger.warning(f"Setting {key} to be '{value}'")
-        elif ggcfg[key] != value:
+        elif cfg[key] != value:
             logger.warning(f"Overriding {key} to be '{value}'")
-        ggcfg[key] = value
+        cfg[key] = value
 
     _override(ggcfg, "whoCanJoin", "INVITED_CAN_JOIN")
     _override(ggcfg, "whoCanViewGroup", "ALL_MEMBERS_CAN_VIEW")
@@ -203,13 +129,13 @@ def main():
 
     summarize_settings(ggcfg)
 
-    SCOPES = [
+    scopes = [
         "https://www.googleapis.com/auth/admin.directory.group",
         "https://www.googleapis.com/auth/admin.directory.group.member",
         "https://www.googleapis.com/auth/apps.groups.settings",
     ]
     creds = service_account.Credentials.from_service_account_file(
-        args.sa_creds, scopes=SCOPES, subject=args.sa_delegate
+        args.sa_creds, scopes=scopes, subject=args.sa_delegate
     )
 
     svc = discovery.build("admin", "directory_v1", credentials=creds, cache_discovery=False)
